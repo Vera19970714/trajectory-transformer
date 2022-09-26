9e957dbad55263362f57adaa20c83dbc49eda01c
Yini Fang <adia@xiaokeai.local>
2022-09-26 20:01:32 +0800

    data process debug: save numpy not tensor, delete some for loops

--- src/preprocess/img_feature.py
+++ src/preprocess/img_feature.py
@@ -1,13 +1,10 @@
-import torch
-import xlrd
-import torchvision.transforms as transforms
-import os
+#import torch
+#import torchvision.transforms as transforms
 import pandas as pd
-from pandas import DataFrame
 import numpy as np
 import cv2 as cv
-
-
+from collections import Counter
+import pickle
 img_dir = './dataset/img/Question/'
 data_dir = './dataset/gaze/whole_target_correct_time.xlsx'
 target_dir = './dataset/img/Target/'
@@ -26,31 +23,26 @@ class CUT_PIC(object):
         dataset2 = []
         dataset3 = []
         words = [str(item) for item in list(self.df["id"])]
-        word_dict_sorted = {}
-            
-        for line in words:
-            line = line.replace(',',' ').replace('\n',' ').lower()
-            for word in line.split():
-                if word in word_dict_sorted:
-                    word_dict_sorted[word] += 1
-                else:
-                    word_dict_sorted[word] = 1
+        #word_dict_sorted = {}
+
+        word_dict_sorted2 = Counter(words)
         
-        for key in word_dict_sorted:
-            id_num = word_dict_sorted[key]
+        for key in word_dict_sorted2:
+            #id_num = word_dict_sorted[key]
             df1 = self.df[self.df["id"]==int(key)]
             df1.reset_index(drop=True, inplace=True)
             question_name = list(df1["question"])[0]
             package_seq = list(df1["package"])
             package_target = list(df1["target_package"])[0]
-            package_target  = list(np.repeat(package_target,27))
-            loader = transforms.ToTensor()
+            package_target  = [package_target]  # (np.repeat(package_target,27))
+            #loader = transforms.ToTensor()
             
             dataset_dict = {}
             Question_img_feature = []
             
             
             if question_name.startswith('Q1'):
+                continue
                 IMAGE_SIZE_1 = 449
                 IMAGE_SIZE_2 = 152
                 IMAGE_ROW = 2
@@ -88,23 +80,26 @@ class CUT_PIC(object):
                     
                     # img_cropped_feature = img_cropped_feature[int((IMAGE_SIZE_1 - CROP_RANGE_1) / 2) : int(IMAGE_SIZE_1 - (IMAGE_SIZE_1 - CROP_RANGE_1) / 2), int((IMAGE_SIZE_2 - CROP_RANGE_2) / 2) : int(IMAGE_SIZE_2 - (IMAGE_SIZE_2 - CROP_RANGE_2)/2)]
                     img_cropped_feature = cv.resize(img_cropped_feature, dim)
-                    img_cropped_feature = loader(img_cropped_feature).unsqueeze(0)
+                    #img_cropped_feature = loader(img_cropped_feature).unsqueeze(0)
                     Question_img_feature.append(img_cropped_feature)
                     
             dataset_dict['package_target'] = package_target
             dataset_dict['package_seq'] =  package_seq
             dataset_dict['question_img_feature'] =  Question_img_feature
 
-            if question_name.startswith('Q1'):    
-                dataset1.append(dataset_dict)
-            elif question_name.startswith('Q2') or question_name.startswith('Q3'):    
+            #if question_name.startswith('Q1'):
+            #    dataset1.append(dataset_dict)
+            if question_name.startswith('Q2') or question_name.startswith('Q3'):
                 dataset2.append(dataset_dict)
             # elif question_name.startswith('Q3'):    
             #     dataset3.append(dataset_dict)
 
-        torch.save(dataset1, "./dataset/processdata/dataset_Q1_time")
-        torch.save(dataset2, "./dataset/processdata/dataset_Q23_time")
+        #torch.save(dataset1, "./dataset/processdata/dataset_Q1_time")
+        #np.save(dataset2, "./dataset/processdata/dataset_Q23_time")
         # torch.save(dataset3, "./dataset/processdata/dataset_Q3")
+        with open("./dataset/processdata/dataset_Q23_time", "wb") as fp:  # Pickling
+            pickle.dump(dataset2, fp)
+
         print("Finish...")
 
 if __name__ == '__main__':
--- src/preprocess/randsplit.py
+++ src/preprocess/randsplit.py
@@ -1,34 +1,34 @@
-import torch
+#import torch
 import numpy as np
+import pickle
 
 file = './dataset/processdata/dataset_Q23_time'
 train_num_no = 688
 
 def randsplit(file):
-    raw_data = torch.load(file)
+    with open(file, "rb") as fp:  # Unpickling
+        raw_data = pickle.load(fp)
+    #raw_data = torch.load(file)
     data_length = len(raw_data)
     print(F'len = {data_length}')
     
-    list = [i for i in range(data_length)]
+    list = np.arange(data_length)
     np.random.shuffle(list)
-    
-    traindata = []
-    valdata = []
-    for i in range(data_length):
-        dataindex = list[i]
-        if i < train_num_no:
-            train = raw_data[dataindex]
-            traindata.append(train)
-        if i >= train_num_no:
-            val = raw_data[dataindex]
-            valdata.append(val)
+    train_index = list[:int(data_length*0.9)]
+    test_index = list[-(data_length - int(data_length*0.9)):]
+    traindata = np.array(raw_data)[train_index.astype(int)]
+    valdata = np.array(raw_data)[test_index.astype(int)]
+
     print(len(traindata))
     print(len(valdata))
-    with open('splitlist_time.txt', 'w') as F:
+    with open('./dataset/processdata/splitlist_time.txt', 'w') as F:
         F.writelines([str(item)+'\n' for item in list])
         F.close()
-    torch.save(traindata, "./dataset/splitdata/dataset_Q23_time_train")
-    torch.save(valdata, "./dataset/splitdata/dataset_Q23_time_val")
+    with open("./dataset/processdata/dataset_Q23_time_train", "wb") as fp:  # Pickling
+        pickle.dump(traindata, fp)
+    with open("./dataset/processdata/dataset_Q23_time_val", "wb") as fp:  # Pickling
+        pickle.dump(valdata, fp)
+
 if __name__ == '__main__':
     randsplit(file)
     
