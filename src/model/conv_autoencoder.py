621e96cb83155f4018d83eaa01dfcf38c830e793
Jingling YU <you@example.com>
2022-11-21 00:24:34 +0800

    add baseline test_gt, test_max

--- src/model/conv_autoencoder.py
+++ src/model/conv_autoencoder.py
@@ -373,7 +373,7 @@ class BaseModel(pl.LightningModule):
         src_img = src_img.to(DEVICE)
         tgt_pos = tgt_pos.to(DEVICE)
         tgt_img = tgt_img.to(DEVICE)
-
+        
         tgt_input = tgt_pos[:-1, :]
         tgt_img = tgt_img[:, :-1, :, :, :]
        
@@ -429,7 +429,35 @@ class BaseModel(pl.LightningModule):
         return loss
 
     def test_max(self,src_pos, question_img, src_img, tgt_pos, tgt_img):
-        
+        tgt_input = tgt_pos[:-1, :]
+        tgt_img = tgt_img[:, :-1, :, :, :]
+        length = tgt_pos.size(0)
+        loss = 0
+        for i in range(1,length):
+            if i==1:
+                tgt_input = tgt_pos[:i, :]
+                tgt_img_input = tgt_img[:, :i, :, :, :]
+                # src: 15, b; tgt_input: 14, b; src_msk: 15, 15; tgt_msk: 13, 13; tgt_padding_msk: 2, 13; src_padding_msk: 2, 15
+                logits = self.model(tgt_input, src_img,
+                            question_img, tgt_img_input)
+                tgt_out = tgt_pos[:2, :]
+                _, predicted = torch.max(logits[:,-1,:], 1)
+                print(predicted)
+                loss1 = self.loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1).long())
+                next_tgt_input = torch.cat((tgt_input, predicted.view(-1, 1)), dim=0)
+            else:
+                # tgt_input = next_tgt_input
+                tgt_input = tgt_pos[:i, :]
+                tgt_img_input = tgt_img[:, :i, :, :, :]
+                logits = self.model(tgt_input, src_img,
+                            question_img, tgt_img_input)
+                tgt_out = tgt_pos[i, :]
+                _, predicted = torch.max(logits[:,-1,:], 1)
+                print(predicted)
+                loss += self.loss_fn(logits[:,-1,:].reshape(-1, logits[:,-1,:].shape[-1]), tgt_out.reshape(-1).long())
+                next_tgt_input = torch.cat((next_tgt_input, predicted.view(-1,1)), dim=0)
+        loss = (2*loss1+loss) / (i+1)
+        return loss
 
     def test_step(self, batch, batch_idx):
         src_pos, question_img, src_img, tgt_pos, tgt_img = batch
@@ -439,7 +467,8 @@ class BaseModel(pl.LightningModule):
         src_img = src_img.to(DEVICE)
         tgt_pos = tgt_pos.to(DEVICE)
         tgt_img = tgt_img.to(DEVICE)
-        loss = self.test_gt(src_pos, question_img, src_img, tgt_pos, tgt_img)
+        # loss = self.test_gt(src_pos, question_img, src_img, tgt_pos, tgt_img)
+        loss = self.test_max(src_pos, question_img, src_img, tgt_pos, tgt_img)
         self.log('testing_loss', loss, on_step=True, on_epoch=True, prog_bar=True, sync_dist=True)
         return {'loss': loss, }
 
