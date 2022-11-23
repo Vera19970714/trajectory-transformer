fb40dee306b5b3620d37fcf5d9be163f7bddd802
Jingling YU <you@example.com>
2022-11-23 20:47:38 +0800

    add benchmark_similarity databilder to dataloader

--- src/data_builder.py
+++ src/data_builder.py
@@ -283,3 +283,112 @@ def collate_fn_base(data):
     # torch.Size([b, 1, 150, 93, 3])
     # torch.Size([12, b])
     # torch.Size([b, 12, 150, 93, 3])
+
+
+# ==================================== dataloader for basesimilarity model ===================================
+
+class SimilarityFixDataset(Dataset):
+    def __init__(self, args, new_datapath):
+        #raw_data = torch.load(new_datapath)
+        with open(new_datapath, "rb") as fp:  # Unpickling
+            raw_data = pickle.load(fp)
+        self.data_length = len(raw_data)
+        print(F'len = {self.data_length}')
+        self.package_seq = []
+        self.package_similarity = []
+        self.package_saliency = []
+        self.args = args
+
+        for item in raw_data:
+            self.package_seq.append(item['package_seq'])
+            self.package_similarity.append(item['package_similarity'])
+            self.package_saliency.append(item['package_saliency'])
+            
+        self.data_total_length = len(self.package_seq)
+        
+        print(F'total_len = {self.data_total_length}')
+        
+       
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.package_seq[index],self.package_similarity[index], self.package_saliency[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.data_total_length
+
+    def get_lens(self, sents):
+        return [len(sent) for sent in sents]
+
+
+# Create a dataloading module as per the PyTorch Lightning Docs
+class SimilaritySearchDataModule(pl.LightningDataModule):
+    def __init__(self, args):
+        super().__init__()
+        train_set = SimilarityFixDataset(args, args.train_datapath)
+        val_set = SimilarityFixDataset(args, args.valid_datapath)
+        test_set = SimilarityFixDataset(args, args.test_datapath)
+
+        self.train_loader = DataLoader(dataset=train_set,
+                                        batch_size=args.batch_size,
+                                        num_workers=2,
+                                        collate_fn=collate_fn_similarity,
+                                        shuffle=True)
+        self.val_loader = DataLoader(dataset=val_set,
+                                        batch_size=1,
+                                        num_workers=2,
+                                        collate_fn=collate_fn_similarity,
+                                        shuffle=False)
+        self.test_loader = DataLoader(dataset=test_set,
+                                        batch_size=1,
+                                        num_workers=2,
+                                        collate_fn=collate_fn_similarity,
+                                        shuffle=False)
+
+    def train_dataloader(self):
+        return self.train_loader
+
+    def val_dataloader(self):
+        return self.val_loader
+
+    def test_dataloader(self):
+        return self.test_loader
+
+
+def collate_fn_similarity(data):
+    package_seq = []
+    package_similarity = []
+    package_saliency = []
+
+    for data_entry in data:
+        img_similarity = data_entry[1]  
+        img_saliency = data_entry[2]
+        gaze_seq = data_entry[0] #9,int
+        gaze_seq = np.stack([gaze_seq]) - 1 #tgt, from 0-26
+        gaze_seq = torch.from_numpy(gaze_seq).squeeze(0)
+    
+        gaze_seq = torch.cat((torch.tensor([BOS_IDX]),
+                gaze_seq,
+                torch.tensor([EOS_IDX]))) # bos, gaze, eos
+        package_seq.append(gaze_seq)
+    
+        img_similarity = np.stack([img_similarity])
+        img_similarity = torch.from_numpy(img_similarity).squeeze(0)
+        package_similarity.append(img_similarity)
+        
+        img_saliency = np.stack([img_saliency])
+        img_saliency = torch.from_numpy(img_saliency).squeeze(0)
+        package_saliency.append(img_saliency)
+
+        
+    package_seq = pad_sequence(package_seq, batch_first=False, padding_value=PAD_IDX)
+    package_similarity = torch.stack(package_similarity)
+    package_saliency = torch.stack(package_saliency)
+    
+    return package_similarity, package_saliency, package_seq
+    # torch.Size([1, b])
+    # torch.Size([b, 450, 837, 3])
+    # torch.Size([b, 1, 150, 93, 3])
+    # torch.Size([12, b])
+    # torch.Size([b, 12, 150, 93, 3])
