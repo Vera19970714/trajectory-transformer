2dbd3948cac3f135711eef61fa1e439cb446a72a
Yini Fang <adia@xiaokeai.local>
2023-02-07 21:19:37 +0800

    debug

--- src/run.py
+++ src/run.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('-use_threedimension', type=str, default='True') #NOT USED IN MIT1003
 
     # training settings
-    parser.add_argument('-gpus', default='0', type=str)
+    parser.add_argument('-gpus', default='1', type=str)
     parser.add_argument('-batch_size', type=int, default=2)
     parser.add_argument('-num_epochs', type=int, default=500)
     parser.add_argument('-random_seed', type=int, default=3407)
