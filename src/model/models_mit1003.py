b877a14580285a78d1a3768cb8f2518c53dcc6cf
Yini Fang <adia@xiaokeai.local>
2023-04-27 20:00:39 +0800

    add layer norm

--- src/model/models_mit1003.py
+++ src/model/models_mit1003.py
@@ -102,22 +102,11 @@ class Seq2SeqTransformer4MIT1003(nn.Module):
         self.LinearEmbedding = nn.Linear(input_dimension, int(emb_size/2))
         self.isDecoderOutputFea = isDecoderOutputFea
         self.isGlobalToken = isGlobalToken
+        self.laynorm = nn.LayerNorm(emb_size),
         if not self.isDecoderOutputFea:
             self.LinearEmbedding_decoder = nn.Linear(input_dimension, emb_size)
         if self.isGlobalToken:
             self.globalToken = nn.Parameter(torch.randn(1, 1, emb_size))
-
-        '''
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
-            nn.LayerNorm(patch_dim),
-            nn.Linear(patch_dim, dim),
-            nn.LayerNorm(dim),
-        )
-        
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        '''
-
 
     def getCNNFeature(self, src_img: Tensor):
         with torch.no_grad():
@@ -138,13 +127,14 @@ class Seq2SeqTransformer4MIT1003(nn.Module):
         #src_pos_emb = self.src_tok_emb(src) # 28, 4, 256
         src_pos_emb = self.LinearEmbedding(src)
         src_emb = torch.cat((src_cnn_emb, src_pos_emb), dim=2) #28, 1, 384(256+128)
+
         if self.isGlobalToken:
             bs = src_emb.size()[1]
             cls_tokens = self.globalToken.repeat(1, bs, 1) # 1,2,512
             src_emb = torch.cat((cls_tokens, src_emb), dim=0)
         src_emb = self.visual_positional_encoding(src_emb) #28,4,512
         #src_emb = self.positional_encoding(src_emb) #CHANGE: use positional encoding as well
-
+        src_emb = self.laynorm(src_emb)
         if self.isDecoderOutputFea:
             tgt_cnn_emb = self.cnn_embedding(tgt_img).transpose(0, 1)  # 28, 4, 256
             #tgt_pos_emb = self.tgt_tok_emb(trg)  # 28, 4, 256
@@ -152,7 +142,9 @@ class Seq2SeqTransformer4MIT1003(nn.Module):
             tgt_emb = torch.cat((tgt_cnn_emb, tgt_pos_emb), dim=2)
         else:
             tgt_emb = self.LinearEmbedding_decoder(trg)
+        tgt_emb = self.laynorm(tgt_emb)
         tgt_emb = self.positional_encoding(tgt_emb)
+
         outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
                                 src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
         return self.generator(outs)
