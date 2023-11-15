02823ab7694779c5606c55f1c30b9ed8177beb97
Yini Fang <adia@xiaokeai.local>
2023-11-15 19:54:33 +0800

    add experiment commands

---
+++ commands/experiments.sh
@@ -0,0 +1,18 @@
+python3 src/run.py  -log_name train_all_test_all_random_time_original -testing_dataset_choice all -training_dataset_choice all \
+   > train_all_test_all_random_time_original.txt
+
+python3 src/run.py  -log_name train_all_test_all_random_time_original -testing_dataset_choice all -training_dataset_choice all \
+  -functionChoice original > train_all_test_all_random_time_original.txt
+
+python3 src/run.py -log_name train_wine_test_wine_random_time -testing_dataset_choice wine -training_dataset_choice wine \
+       -CA_version 0     > train_wine_test_wine_random_time_fc.txt
+
+python3 src/run.py -log_name train_yogurt_test_yogurt_random_time -testing_dataset_choice yogurt -training_dataset_choice yogurt \
+       -CA_version 0     > train_yogurt_test_yogurt_random_time_fc.txt
+
+
+python3 src/run.py -log_name train_wine_test_wine_random_time -testing_dataset_choice wine -training_dataset_choice wine \
+       > train_wine_test_wine_random_time.txt
+
+python3 src/run.py -log_name train_yogurt_test_yogurt_random_time -testing_dataset_choice yogurt -training_dataset_choice yogurt \
+        > train_yogurt_test_yogurt_random_time.txt
