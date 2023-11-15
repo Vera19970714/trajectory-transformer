a37e35bdc049abc25598cf25f4eb3bdcfa3d0d95
Yini Fang <adia@xiaokeai.local>
2023-11-15 22:06:52 +0800

    add PE path choice in run.py

--- commands/experiments.sh
+++ commands/experiments.sh
@@ -1,9 +1,17 @@
+python3 src/run.py -PE_matrix ./src/model/learned_PE_random_2.npy \
+ -log_name train_all_test_all_random_time > train_all_test_all_random_time_PE2.txt
+
+python3 src/run.py -PE_matrix ./src/model/learned_PE_random_2.5.npy \
+ -log_name train_all_test_all_random_time > train_all_test_all_random_time_PE2.5.txt
+
 python3 src/run.py  -log_name gazeformer  -model Gazeformer > gazeformer.txt
 
 python3 src/run.py  -log_name train_all_test_all_random_time_original  \
   -functionChoice original > train_all_test_all_random_time_original.txt
 
-python3 src/run.py -log_name train_wine_test_wine_random_time -testing_dataset_choice wine -training_dataset_choice wine \
+#python3 src/run.py  -log_name train_all_test_all_random_time > train_all_test_all_random_time.txt
+
+'''python3 src/run.py -log_name train_wine_test_wine_random_time -testing_dataset_choice wine -training_dataset_choice wine \
        -CA_version 0     > train_wine_test_wine_random_time_fc.txt
 
 python3 src/run.py -log_name train_yogurt_test_yogurt_random_time -testing_dataset_choice yogurt -training_dataset_choice yogurt \
@@ -14,4 +22,4 @@ python3 src/run.py -log_name train_wine_test_wine_random_time -testing_dataset_c
        > train_wine_test_wine_random_time.txt
 
 python3 src/run.py -log_name train_yogurt_test_yogurt_random_time -testing_dataset_choice yogurt -training_dataset_choice yogurt \
-        > train_yogurt_test_yogurt_random_time.txt
+        > train_yogurt_test_yogurt_random_time.txt'''
