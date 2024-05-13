9c7a37f78d0330f6c7dc979e9e45eda7032dcade
Yini Fang <adia@xiaokeai.local>
2024-05-13 20:44:57 +0800

    add split sh files

---
+++ commands/amazon_4split.sh
@@ -0,0 +1,6 @@
+python3 src/run.py -data_path ./dataset/processdata/dataset_amazon \
+-index_file splitlist_all_amazon.txt \
+-testing_dataset_choice amazon -training_dataset_choice amazon \
+-batch_size 10 \
+-log_name amazon_split
+
---
+++ commands/pamformer_4split.sh
@@ -0,0 +1,6 @@
+python3 src/run.py -data_path ./dataset/processdata/dataset_Q123_mousedel_time_new \
+-index_file splitlist_all_time.txt \
+-testing_dataset_choice all -training_dataset_choice all \
+-batch_size 10 \
+-log_name pamformer_4split
+
