5532f103d92c93b0fbbfe2592f293422681e9d1b
Yini Fang <adia@xiaokeai.local>
2024-07-09 12:34:32 +0800

    learned choice for decoder PE

--- src/model/models.py
+++ src/model/models.py
@@ -286,21 +286,19 @@ class Seq2SeqTransformer(nn.Module):
                 memory_key_padding_mask: Tensor,
                 dataset=None,
                 patch_in_batch=True):
-        if self.functionChoice != 'e2e':
-            if self.functionChoice != 'original_update' and self.functionChoice != 'learned':
-                threed_pe = self.threedSin
-            elif self.functionChoice != 'learned':
-                if dataset is None:
-                    #threed_pe = self.threedSin
-                    shelf_len = src.size()[0]
-                    if shelf_len == 23:
-                        threed_pe = self.threedSin_wine
-                    elif shelf_len == 28:
-                        threed_pe = self.threedSin_yogurt
-                elif dataset == 0:
-                    threed_pe = self.threedSin_yogurt
-                elif dataset == 1:
+        if self.functionChoice != 'original_update' and self.functionChoice != 'learned':
+            threed_pe = self.threedSin
+        elif self.functionChoice != 'learned':
+            if dataset is None:
+                shelf_len = src.size()[0]
+                if shelf_len == 23:
                     threed_pe = self.threedSin_wine
+                elif shelf_len == 28:
+                    threed_pe = self.threedSin_yogurt
+            elif dataset == 0:
+                threed_pe = self.threedSin_yogurt
+            elif dataset == 1:
+                threed_pe = self.threedSin_wine
         src_cnn_emb = self.cnn_embedding(src_img, patch_in_batch).transpose(0, 1) #28, 4, 256
         if self.functionChoice == 'learned':
             src_pos_emb = calculate3DPositional_learned(self.pe, src, self.pe_embed).to(DEVICE)
@@ -316,7 +314,8 @@ class Seq2SeqTransformer(nn.Module):
         #tgt_pos_emb = self.tgt_tok_emb(trg)  # 28, 4, 256
 
         #tgt_pos_emb = self.LinearEmbedding(trg)
-        if self.functionChoice == 'learned':
+        if self.functionChoice == 'learned' or self.functionChoice == 'e2e':
+            # if choice is e2e, still use learned for decoder
             tgt_pos_emb = calculate3DPositional_learned(self.pe, trg, self.pe_embed).to(DEVICE)
         else:
             tgt_pos_emb = calculate3DPositional(threed_pe, trg).to(DEVICE)
